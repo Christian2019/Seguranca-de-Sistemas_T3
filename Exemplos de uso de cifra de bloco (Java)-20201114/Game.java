c8642ca230c8ee71e3c0b8f2fc9d0fb294a32716
Christian2019 <37154877+Christian2019@users.noreply.github.com>
2020-11-16 22:21:18 -0300

    update

--- Exemplos de uso de cifra de bloco (Java)-20201114/Game.java
+++ Exemplos de uso de cifra de bloco (Java)-20201114/Game.java
@@ -33,20 +33,24 @@ public class Game {
 		BigInteger g = new BigInteger(g_hexadecimal, 16);
 		Random r = new Random();
 		BigInteger a = new BigInteger(p.bitLength() - 1, r);
+		System.out.println("a: "+a);
+		a=new BigInteger("36486017791378609133135453651544745583908701932604171757325000294236012445885989056153579344494292093547059530500035961596075243066248870696174266635872254508959089639391896718353325360111926477994831918057029006526489833308083070914665860626720334849893400536797332583070678236185580789355277838560845430490");
 		BigInteger A = g.modPow(a, p);
 		// Enviar para o Professor
 		String A_hexadecimal = A.toString(16);
-	//	System.out.println(A_hexadecimal);
+		System.out.println("A: "+A_hexadecimal);
 		
 		// Passo 2
 		//B recebido do professor
-		String B_hexadecimal = "3878171495B549C5F40162B5514106546A346EC9552AE3F0F28B8F46B409E9B94EDFCD432EF8A60602CA126C7E6874EB4333EFBA26B9786020492B4D7A4F8FEE74E91504939BA2895C97EAB1698CB42BD506025975E851F5118B8E486293FB395E534116F51603C853BFB475C1F339D47C5FDCC94261147A1FE6D743B626CC32";
+	//	String B_hexadecimal = "00ABBD2BCCB8CFF59B9F10D7F785519ED3AA0E68D00CEBCE587C8ED93A605913B9572418BFDE3D26E1365C75E6040880D880B2960F92597ECC57DED31932AA2099132998A461DE381AAD88A825D1167F89254C828198516F10923CF45986015C6EC9CD43D476D2556BAB70A5204E349EB3ACA232D83397C1EFCD0D452978060D81";
+		//Eduardo
+		String B_hexadecimal ="3786ab2e45fee25bf5aeeab471f0f4577da9e82bdd93c9288c012a72b247f0827e7176abbe5becb5995452503f847435d4dc41d37cece0d9458946309a349ebc443e0c915662fe1d78e8c2227baa7e76cfe1170664baa1b721fb393d57cb5a61e4afa013e45cdeb813bfe24995720192bf70a14842ddfd78cf1d87a2f49ce474";
 		BigInteger B = new BigInteger(B_hexadecimal, 16);
 		BigInteger V = B.modPow(a, p);
 
 		// Passo 3
 
-		byte[] dataBytes = V.toString().getBytes();
+		byte[] dataBytes = V.toByteArray();
 
 		MessageDigest md = null;
 		try {
@@ -55,7 +59,7 @@ public class Game {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		md.update(dataBytes, 0, V.toString().length());
+		md.update(dataBytes, 0, dataBytes.length);
 		byte[] mdbytes = md.digest();
 		
 		byte[] senha = Arrays.copyOfRange(mdbytes, 0, 16);
@@ -63,7 +67,11 @@ public class Game {
 				
 		// Etapa 2
 		// Recebimento da mensagem
-		String mensagemprofessor_hex = "E62968DB1C564B045C9628CDDD2445B70C3F38C9AC1954D6D66C38D71566C61DB1D5B0394E1822C251F08E928C0F855A6B3D6D955B835BFD0986D52B5103855646F2CF54F8FA9E16723F581520B68AA4F01CBDEBBCA1C8DBE96657734E6190FFDF981064D3DA58DAECBDC5734CD07E10";
+		//Professor
+	//	String mensagemprofessor_hex = "582FF060EA980234D69F725492BCFBF7BD9F763637704FCB100BD8E441ACF42E5907440BCAE4EB342C8CB267CF9E0BC920DE29B93F8E72540FE4DE7401389D9007E5DEF13F251F77D7A72B8C58B0F768A6B00B1F360EB767F0F7C07794CC2DABEB3B8F338BA1412A9C46DB622CAA8683";
+		//Eduardo
+		String mensagemprofessor_hex ="31CCE0E356E19458F94A5049490DCD9B1D66981F71ED43F624E4A2D000248DDD";
+		
 		byte[] hash = hexStringToByteArray(mensagemprofessor_hex);
 		IvParameterSpec iv =new IvParameterSpec(Arrays.copyOfRange(hash, 0, 16));
 		byte[] mens = Arrays.copyOfRange(hash, 16, hash.length);
@@ -81,9 +89,33 @@ public class Game {
 		
 		//Enviar Nova mensagem
 		
+		String mensagem_invertida= new StringBuilder(mensagem_textoClaro).reverse().toString();
+		System.out.println(mensagem_invertida);
+		
+		 SecretKeySpec skeySpec2 = new SecretKeySpec(senha, "AES");
+
+         Cipher cipher2 =Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+         cipher2.init(Cipher.ENCRYPT_MODE, skeySpec2);
+
+         byte[] encrypted = cipher2.doFinal(mensagem_invertida.getBytes());
+         
+         System.out.println("encrypted string: " + byteArrayToHexString(encrypted));
 		
 
 	}
+	 public static String byteArrayToHexString(byte[] b)
+	    {
+	        StringBuffer sb = new StringBuffer(b.length * 2);
+	        for (int i = 0; i < b.length; i++) {
+	            int v = b[i] & 0xff;
+	            if (v < 16) {
+	                sb.append('0');
+	            }
+	            sb.append(Integer.toHexString(v));
+	        }
+	        return sb.toString().toUpperCase();
+	    }
 
 	public static byte[] hexStringToByteArray(String s) {
 		int len = s.length();
